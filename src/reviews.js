455f22ba8b112689ea615009c29eeaf90e2c64fe
Corum Gold <goldcorum@gmail.com>
2023-06-12 11:58:45 -0400

    Add chico review

--- src/reviews.js
+++ src/reviews.js
@@ -17,6 +17,15 @@ const reviews = [
       "As a Dog Mom who sent her Littles away to Board and Train, I was very leery. Turns out, it was the best thing I ever did, and I would do it again... Marguerite (“M, The Gem”) is a true miracle worker... I highly recommend Marguerite to anyone who wants a better relationship with their pet. Her depth of knowledge and extensive experience shines through with every dog she trains. The only thing you will regret is not doing it sooner, and you will be amazed by the compliments you and your pet receive.",
     tools: ["Off-Leash Heel", "Behavior Modification", "Confidence-Building", "Leash Pulling"],
   },
+  {
+    id: 3,
+    title: "Chico",
+    image: process.env.PUBLIC_URL + `/images/chico.png`,
+    altText: "Chico sleeping image",
+    description:
+      "Marguerite was extremely patient, and she knew exactly what she was doing. Her experience and expert guidance was the perfect combination for me and Chico. She trained us both! I highly recommend her as does Chico.",
+    tools: ["Behavior Modification", "State of Mind", "Calming", "Place"],
+  },
 ];
 
 export default reviews;
